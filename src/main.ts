449d70f044d372c59e16053c7318621ce9e01621
surajbhoji1208 <surajbhoji1208@gmail.com>
2023-11-23 12:08:51 +0530

    configuration management setup 64

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig=config.get('server')
+  console.log(serverConfig);
+  
   await app.listen(3000);
 }
 bootstrap();
